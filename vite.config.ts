8edac5495c04ac70b72dbcfa0fb3dc890f642a4f
psp <976477689@qq.com>
2024-08-26 20:59:54 +0800

    chore: 移除多余代码

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
-import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue()],
-	build: {
-		lib: {
-			entry: path.resolve(__dirname, "./lib/index.ts"),
-			name: "tiktok",
-			fileName: (format) => `tiktok.${format}.js`,
-		},
-	},
 })
